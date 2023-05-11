9c374eda0c597016c689cd8bdeb40d8e7703d9e2
NandhiniSundararajan <nandysundar@gmail.com>
2023-05-11 18:00:15 -0400

    First Commit

---
+++ src/test/java/MavenPack/MvnTests.java
@@ -0,0 +1,21 @@
+package MavenPack;
+
+import org.testng.annotations.Test;
+
+public class MvnTests {
+
+    @Test
+    public void testOne(){
+        System.out.println("Test one");
+    }
+
+    @Test
+    public void testTwo(){
+        System.out.println("Test two");
+    }
+
+    @Test
+    public void testThree(){
+        System.out.println("Test three");
+    }
+}
